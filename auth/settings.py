c437f224e6cb0ad8db67ae293ee576478a3db5e3
sukham0206 <schatha_be20@thapar.edu>
2022-07-19 16:56:44 +0530

    commit commit

--- auth/settings.py
+++ auth/settings.py
@@ -39,7 +39,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'home'
+    'home',
+    'whitenoise.runserver_nostatic'
 ]
 
 MIDDLEWARE = [
