172c0ebb2f22efee5cff8d98167a3fb85423d522
sukham0206 <schatha_be20@thapar.edu>
2022-07-19 16:58:34 +0530

    commit

--- auth/settings.py
+++ auth/settings.py
@@ -78,6 +78,8 @@ WSGI_APPLICATION = 'auth.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/4.0/ref/settings/#databases
 
+import dj_database_url
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
@@ -87,7 +89,8 @@ DATABASES = {
         'HOST': 'localhost',
     }
 }
-
+db_from_env = dj_database_url.config(conn_max_age=600)
+DATABASES['default'].update(db_from_env)
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
