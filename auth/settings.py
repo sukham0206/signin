76cba5a64fcd383de246ebdac46bc29b06cdf083
sukham0206 <schatha_be20@thapar.edu>
2022-07-19 16:55:37 +0530

    commit

--- auth/settings.py
+++ auth/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY = config('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'https://projlogin.herokuapp.com/']
 
 
 # Application definition
